0ce71fb2983abeab41c19b2ef8a959d7adc80799
Christian Wiedemann <christian.wiedemann@key-tec.de>
2020-10-14 19:16:06 +0200

    Add docksal settings.

--- docroot/sites/default/settings/docksal.settings.php
+++ docroot/sites/default/settings/docksal.settings.php
@@ -10,17 +10,24 @@ $dir = dirname(DRUPAL_ROOT);
 $config['system.file']['path']['temporary'] = '/tmp';
 $settings['file_private_path'] = $dir . '/files-private';
 
-$databases = array(
-  'default' => array(
-    'default' => array(
-      'database' => 'default',
-      'username' => 'user',
-      'password' => '9Q8y+_CKolk34',
-      'host' => 'db',
-      'port' => '3306',
-      'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
-      'driver' => 'mysql',
-      'prefix' => '',
-    ),
-  ),
-);
+$db_name = 'default';
+/**
+ * Database configuration.
+ */
+$databases = [
+  'default' =>
+    [
+      'default' =>
+        [
+          'database' => $db_name,
+          'username' => 'root',
+          'password' => '9Q8y+_Ckh32lk34',
+          'host' => 'db',
+          'port' => '3306',
+          'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+          'driver' => 'mysql',
+          'prefix' => '',
+        ],
+    ],
+];
+
