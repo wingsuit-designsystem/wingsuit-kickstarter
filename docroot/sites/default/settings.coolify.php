fb3710cb5087d0569b08f10eb500aa201c97ec7d
Christian Wiedemann <christian.wiedemann@key-tec.de>
2024-12-13 12:31:13 +0100

    Adjust variables

--- docroot/sites/default/settings.coolify.php
+++ docroot/sites/default/settings.coolify.php
@@ -8,19 +8,13 @@
 
 
 $wodby['php_storage_dir'] = getenv("DRUPAL_PHP_STORAGE_DIR");
-$wodby['site'] = getenv("DRUPAL_SITE");
 $wodby['hash_salt'] = getenv("DRUPAL_HASH_SALT");
 $wodby['sync_salt'] = getenv('DRUPAL_FILES_SYNC_SALT');
 
-$wodby['db']['host'] = getenv('DB_HOST');
-$wodby['db']['port'] = getenv('DB_PORT');
-$wodby['db']['name'] = getenv('DB_NAME');
-if (getenv("DB_USERNAME")) {
-  $wodby['db']['username'] = getenv("DB_USERNAME");
-}
-$wodby['db']['username'] = getenv("DB_USER");;
-
-
+$wodby['db']['host'] = getenv('DB_HOST') ?? 'db';
+$wodby['db']['port'] = getenv('DB_PORT') ?? '3306';
+$wodby['db']['name'] = getenv('DB_NAME') ?? 'drupal';
+$wodby['db']['username'] = getenv("DB_USERNAME");
 $wodby['db']['password'] = getenv("DB_PASSWORD");
 $wodby['db']['driver'] = "mysql";
 
